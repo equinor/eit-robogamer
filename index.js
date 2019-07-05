87ee55e6a0112049b84536951720047aeafdf522
Ole Martin Gjersvik <olgj@equinor.com>
2019-07-05 12:59:10 +0200

    Installed Socket.io

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const socket = require('socket.io');
+const http = require('http');
 const app = express();
+const server = http.createServer(app);
 const port = 8080
+const io = socket(server);
 
-app.use(express.static("public"))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.use(express.static("public"));
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+});
+
+server.listen(port, () => console.log(`Example app listening on port ${port}!`))
