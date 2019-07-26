8461bd2fe662d4611c728797aead21da41bd5c4b
Ole Martin Gjersvik <olgj@equinor.com>
2019-07-26 10:10:38 +0200

    Added expected values.

--- src/BotPhysics.ts
+++ src/BotPhysics.ts
@@ -1,7 +1,7 @@
 export class BotPos {
-    x: number;
-    y: number;
-    angle: number;
+    x: number; //from 0.0 to 16.0
+    y: number; //from 0.0 to 9.0
+    angle: number; // radians
 
     constructor(x = 0,y = 0, angle = 0){
         this.x = x;
