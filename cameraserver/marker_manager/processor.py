185be672d722d140e2b16f587d7f19a7fe15cd08
Li Deng <LIDE@equinor.com>
2019-08-09 11:12:28 +0200

    Add camera tracking

--- cameraserver/marker_manager/processor.py
+++ cameraserver/marker_manager/processor.py
@@ -6,23 +6,29 @@ import matplotlib as mpl
 import pandas as pd
 import math
 
-frame = cv2.imread("../marker_images/hamming180.jpg")
+
+frame = cv2.imread("../marker_images/markers.jpg")
 plt.figure()
 plt.imshow(frame)
 
 gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
+aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_100)
 parameters =  aruco.DetectorParameters_create()
 corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
-frame_markers = aruco.drawDetectedMarkers(frame.copy(), corners, ids)
-df = pd.DataFrame()
+df = pd.DataFrame(columns=['id', 'px', 'py', 'radian'])
 for i in range(len(ids)-1, -1, -1):
     c = corners[i][0]
     px, py = c[:, 0].mean(), c[:, 1].mean()
     delta_y = c[1, 1] - c[0, 1]
     delta_x = c[1, 0] - c[0, 0]
     angleInRadian = math.atan2(delta_y, delta_x)
+    print(type(ids[i][0]))
+    df = df.append({'id': int(ids[i][0]), 'px': px, 'py': py, 'radian': angleInRadian}, ignore_index=True)
+    
     print(angleInRadian)
     plt.plot([c[:, 0].mean()], [c[:, 1].mean()], "o", label = "id={0}".format(ids[i]))
-plt.legend()
-plt.show()
+
+df['id'] = df['id'].astype(np.int64)
+print(df)
+#plt.legend()
+#plt.show()
