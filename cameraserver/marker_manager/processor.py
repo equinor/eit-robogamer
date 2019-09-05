bf8552b842d03d3b508b260cbec58451bab48a41
Li Deng <LIDE@equinor.com>
2019-09-05 10:42:19 +0200

    Resize option for the camera frame

--- cameraserver/marker_manager/processor.py
+++ cameraserver/marker_manager/processor.py
@@ -8,7 +8,7 @@ import pandas as pd
 import math
 
 
-frame = cv2.imread("../marker_images/markers_xcolor.jpg")
+frame = cv2.imread("../marker_images/markers.jpg")
 height, width = frame.shape[:2]
 unit = width/16
 print(height, width)
@@ -19,18 +19,20 @@ gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_100)
 parameters =  aruco.DetectorParameters_create()
 corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
-df = pd.DataFrame(columns=['id', 'px', 'py', 'radian'])
+#df = pd.DataFrame(columns=['id', 'px', 'py', 'radian'])
+df=[]
 for i in range(len(ids)-1, -1, -1):
     c = corners[i][0]
-    px, py = c[:, 0].mean(), c[:, 1].mean()
+    px, py = c[:, 0].mean()/unit, c[:, 1].mean()/unit
     delta_y = c[1, 1] - c[0, 1]
     delta_x = c[1, 0] - c[0, 0]
     angleInRadian = math.atan2(delta_y, delta_x)
-    df = df.append({'id': int(ids[i][0]), 'px': px/unit, 'py': py/unit, 'radian': angleInRadian}, ignore_index=True)
+    #df = df.append({'id': int(ids[i][0]), 'px': px/unit, 'py': py/unit, 'radian': angleInRadian}, ignore_index=True)
+    df.extend([ids[i][0], px, py, angleInRadian])
     
     plt.plot([c[:, 0].mean()], [c[:, 1].mean()], "o", label = "id={0}".format(ids[i]))
 
-df['id'] = df['id'].astype(np.int64)
+#df['id'] = df['id'].astype(np.int64)
 print(df)
 plt.legend()
 plt.show()
--- cameraserver/marker_manager/scaner.py
+++ cameraserver/marker_manager/scaner.py
@@ -7,8 +7,8 @@ import numpy as np
 
 
 def get_cordinates(ids, corners, unit):
-    print(ids)
-    df = pd.DataFrame(columns=['id', 'px', 'py', 'radian'])
+    df = []
+    #df = pd.DataFrame(columns=['id', 'px', 'py', 'radian'])
     try:
         if ids.any():
             try:        
@@ -18,32 +18,36 @@ def get_cordinates(ids, corners, unit):
                     delta_y = c[1, 1] - c[0, 1]
                     delta_x = c[1, 0] - c[0, 0]
                     angleInRadian = math.atan2(delta_y, delta_x)
-                    df = df.append({'id': ids[i][0], 'px': px/unit, 'py': py/unit, 'radian': angleInRadian}, ignore_index=True)    
+                    df.extend([ids[i][0], px/unit, py/unit, angleInRadian])
+                    #df = df.append({'id': ids[i][0], 'px': px/unit, 'py': py/unit, 'radian': angleInRadian}, ignore_index=True)    
             except:
                 pass
     except:
-        return
-    df['id'] = df['id'].astype(np.int64)
+        df = ""
+        #return
+    #df['id'] = df['id'].astype(np.int64)
     return df
 
 
 if __name__ == '__main__':
     capture = cv2.VideoCapture(1)
     aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_100)
+    # capture.set(3, 1280)
+    # capture.set(4, 720)
     
     if capture.isOpened():
         frame_captured, frame = capture.read()
     else:
         frame_captured = False
     while frame_captured:
-        # width = capture.get(cv2.CV_CAP_PROP_FRAME_WIDTH)   # float
-        # height = capture.get(cv2.CV_CAP_PROP_FRAME_HEIGHT) # float
         height, width = frame.shape[:2]
+        print(height, width)
         unit = width/16
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         parameters =  aruco.DetectorParameters_create()
         corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
-        df = get_cordinates(ids, corners)
+        df = get_cordinates(ids, corners, unit)
+        df = "|".join(str(x) for x in df)
         print(df)
         #frame_markers = aruco.drawDetectedMarkers(frame.copy(), corners, ids)
         #cv2.imshow('Captured Frame', frame)
