7226f3c3c4adce5c47c0ef679302f60ffff93e6c
Ole Martin Gjersvik <olgj@equinor.com>
2019-09-12 10:22:26 +0200

    Written tests for Cli.

--- src/Cli.ts
+++ src/Cli.ts
@@ -1,22 +1,23 @@
 import Options from "./models/Options";
-import commander from 'commander';
+import {Command} from 'commander';
 
 export function parseArgs(args: string[]): Options {
-    commander.version('0.0.1');
-    commander
+    var program = new Command();
+    program.version('0.0.1');
+    program
         .option('-r, --red-team <file>', 'The program to run as the red team')
         .option('-b, --blue-team <file>', 'The program to run as the blue team')
         .option('--sim', 'Run in simulator. Default')
         .option('--real', 'Run using real robots');
     
-    commander.parse(args);
+        program.parse(args);
 
     var options = Options.default;
 
-    if(commander.redTeam) options = options.setRed(commander.redTeam);
-    if(commander.blueTeam) options = options.setBlue(commander.blueTeam);
-    if(commander.real) options = options.setReal();
-    if(commander.sim) options = options.setSim();
+    if(program.redTeam) options = options.setRed(program.redTeam);
+    if(program.blueTeam) options = options.setBlue(program.blueTeam);
+    if(program.real) options = options.setReal();
+    if(program.sim) options = options.setSim();
 
     return options;
 }
