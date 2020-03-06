5d4d4987a395cc2b947c96ee99eb0e7e99c4a375
Ole Martin Gjersvik <olgj@equinor.com>
2020-03-06 17:07:15 +0100

    Minimal camera library that works.

--- camera/src/main.rs
+++ camera/src/main.rs
@@ -1,26 +1,21 @@
-use rscam::{Camera, Config, Frame};
-use image::imageops::colorops::grayscale;
-use std::time::{Duration, Instant};
+use async_std::sync::channel;
+use camera::Camera;
+use std::time::Instant;
+use async_std::println;
 
-fn main() {
-    let mut camera = Camera::new("/dev/video0").unwrap();
+#[async_std::main]
+async fn main() -> std::io::Result<()> {
+    let (s, r) = channel(10);
 
-    camera.start(&Config {
-        interval: (1, 30),      // 30 fps.
-        resolution: (1920, 1080),
-        format: b"RGB3",
-        ..Default::default()
-    }).unwrap();
+    let camera = Camera::new(s);
+    camera.start_camera();
 
     let now = Instant::now();
-    let mut duration = Duration::from_secs(0);
     for _ in 0..60 {
-        let frame = camera.capture().unwrap();
-        let loopnow = Instant::now();
-        let image: image::ImageBuffer<image::Rgb<u8>, Frame> = image::ImageBuffer::from_raw(frame.resolution.0, frame.resolution.1, frame).unwrap();
-        let _gray = grayscale(&image);
-        duration += loopnow.elapsed();
+        let frame = r.recv().await.unwrap();
+        println!("{}", frame.id).await;
     }
-    println!("{}", now.elapsed().as_secs_f32());
-    println!("{}", duration.as_secs_f32());
+    println!("{}", now.elapsed().as_secs_f32()).await;
+
+    Ok(())
 }
