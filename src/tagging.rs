dc2af0c1eb13fad7406d1028454feadd22f90e9a
Ole Martin Gjersvik <olgj@equinor.com>
2020-03-17 05:20:22 +0100

    At least it runs.

--- src/tagging.rs
+++ src/tagging.rs
@@ -1,10 +1,20 @@
-use apriltag_sys::{
-    apriltag_detector_create,
-    apriltag_detector_destroy
-};
+use apriltag_sys::*;
+use std::ffi::CString;
+
 
 pub fn tagtest() {
-    let td = unsafe {apriltag_detector_create()};
+    unsafe {
+        let file = CString::new("test.pnm").expect("CString::new failed");
+        let im = image_u8_create_from_pnm(file.as_ptr());
+        let td = apriltag_detector_create();
+        let tf = tag36h11_create();
+        apriltag_detector_add_family_bits(td, tf, 1);
+        let detections = apriltag_detector_detect(td, im);
+        
+        println!("Number of decetions: {}", &mut (*detections).size);
 
-    unsafe {apriltag_detector_destroy(td)};
+        // Cleanup.
+        tag36h11_destroy(tf);
+        apriltag_detector_destroy(td);
+    }
 }
